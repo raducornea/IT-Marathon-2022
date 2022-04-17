42e62c899ff8778a313e281e13644a9e8a4a5d00
Radu Cornea <cornearaduvalentin@gmail.com>
2022-04-17 14:20:10 +0300

    Addes some Contrrollers and Services

--- Kotlin/src/main/kotlin/com/example/itmarathon/services/ServerService.kt
+++ Kotlin/src/main/kotlin/com/example/itmarathon/services/ServerService.kt
@@ -1,33 +1,38 @@
 package com.example.itmarathon.services
 
+import com.example.itmarathon.interfaces.IServerService
 import org.springframework.stereotype.Service
 
 @Service
-class ServerService {
-    fun registerNewUser(userName: String, password: String){
+class ServerService : IServerService {
+    override fun registerNewUser(userName: String, password: String){
 
     }
 
-    fun registerNewDevice(deviceName: String){
+    override fun registerNewDevice(deviceName: String){
 
     }
 
-    fun defineApplication(id: Int, name: String, information: String, version: Int){
+    override fun defineApplication(id: Int, name: String, information: String, version: Int){
 
     }
 
     /** In functie de deviceId, se va notifica user-ul cu privire la noi versiuni pe care le poate sau nu updata */
-    fun notifyUpdates(deviceId: Int){
+    override fun notifyUpdates(deviceId: Int, application: Application){
+
+    }
+
+    override fun notifyDeletion(deviceId: Int, application: Application){
 
     }
 
     /** Vine un nou update */
-    fun uploadVersionSoftwareFiles(id: Int, name: String, information: String, version: Int){
+    override fun uploadVersionSoftwareFiles(id: Int, name: String, information: String, version: Int){
         /** Trebuie notificati utilizatorii care folosesc acea aplicatie */
     }
 
     /** Se sterge o Aplicatie cu totul */
-    fun deleteApplication(id: Int, name: String, information: String, version: Int){
+    override fun deleteApplication(id: Int, name: String, information: String, version: Int){
         /** Trebuie notificati utilizatorii care folosesc acea aplicatie */
     }
 }
