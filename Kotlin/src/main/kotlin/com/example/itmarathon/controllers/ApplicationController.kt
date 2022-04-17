74422d4bb8d8fc33088bf5e8abdc49cb7db6f380
Radu Cornea <cornearaduvalentin@gmail.com>
2022-04-17 14:20:10 +0300

    Addes some Contrrollers and Services

--- Kotlin/src/main/kotlin/com/example/itmarathon/controllers/ApplicationController.kt
+++ Kotlin/src/main/kotlin/com/example/itmarathon/controllers/ApplicationController.kt
@@ -1,5 +1,50 @@
 package com.example.itmarathon.controllers
 
+import com.example.itmarathon.interfaces.IServerService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
 class ApplicationController {
+    @Autowired
+    private lateinit var _serverService: IServerService
+
+    @RequestMapping(value = ["/user"], method = [RequestMethod.POST])
+    fun registerNewUser(@RequestBody user: User): ResponseEntity<Unit> {
+        _serverService.registerNewUser(user)
+        return ResponseEntity(Unit, HttpStatus.CREATED)
+    }
+
+    @RequestMapping(value = ["/device"], method = [RequestMethod.POST])
+    fun registerNewDevice(@RequestBody device: Device): ResponseEntity<Unit> {
+        _serverService.registerNewDevice(device)
+        return ResponseEntity(Unit, HttpStatus.CREATED)
+    }
+
+    @RequestMapping(value = ["/application"], method = [RequestMethod.POST])
+    fun defineApplication(@RequestBody application: Application): ResponseEntity<Unit> {
+        _serverService.registerNewDevice(application)
+        return ResponseEntity(Unit, HttpStatus.CREATED)
+    }
+
+    @RequestMapping(value = ["/application/{id}"], method = [RequestMethod.PUT])
+    fun updateSoftwareVersion(@PathVariable id: Int, @RequestBody application: Application): ResponseEntity<Unit> {
+        _serverService.getApplication(id)?.let {
+            _serverService.notifyUpdates(it.id, application)
+            return ResponseEntity(Unit, HttpStatus.ACCEPTED)
+        } ?: return ResponseEntity(Unit, HttpStatus.NOT_FOUND)
+    }
+
+    @RequestMapping(value = ["/application/{id}"], method = [RequestMethod.DELETE])
+    fun deletePerson(@PathVariable id: Int, @RequestBody application: Application): ResponseEntity<Unit> {
+        if (_serverService.getApplication(id) != null) {
+            _serverService.notifyDeletion(it.id, application)
+            return ResponseEntity(Unit, HttpStatus.OK)
+        } else {
+            return ResponseEntity(Unit, HttpStatus.NOT_FOUND)
+        }
+    }
 
 }
